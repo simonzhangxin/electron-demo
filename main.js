fb481e38b4ebaa4ee33cbe799d5b769eda16edc0
Xin <xin.zhang@innovela.com>
2017-08-11 09:21:36 +0200

    electron updater

--- main.js
+++ main.js
@@ -41,18 +41,19 @@ function createWindow () {
   //mainWindow.webContents.openDevTools()
 
    // check update
+   //Can't call updater unless app is installed !!!
    const page = mainWindow.webContents;
    page.once('did-frame-finish-load', appUpdater);
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+   // Emitted when the window is closed.
+   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  });
+     mainWindow = null
+   });
 
-  require('./menu/mainmenu')
+  require('./menubar')
 }
 
 // This method will be called when Electron has finished
